368b9bdc646c30cb65ac67778e4aa25fff9605e9
Adewole Feyijimi <feyijimi@tenforward.co>
2018-09-02 13:32:16 +0100

    Add files via upload

---
+++ surebackup_sample.ps1
@@ -0,0 +1,22 @@
+﻿# Load Veeam snapin
+Add-PsSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue
+ 
+Clear-Host
+
+# Connect to VBR server
+$w = Get-VBRServerSession
+if (!$w)
+{
+    $Credentials=IMPORT-CLIXML C:\scripts\xml\SecureCredentials.xml
+    Connect-VBRServer -Credential $Credentials
+}
+
+$SureJob = Get-VSBJob -Name "1.     DB-OFCUBS <backup>"
+
+$SureJobState=$SureJob.FindLastSession().State
+
+if ($SureJobState -eq "Working")
+{
+WRITE-HOST "The sessionID for this job is: " $SureJob.Id
+}
+
